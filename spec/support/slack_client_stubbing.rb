d25c5387648f7c8043910119fcac555e00d23e05
Jamie Wright <jamie@brilliantfantastic.com>
2017-02-27 08:33:21 -0500

    Record the conversations

--- spec/support/slack_client_stubbing.rb
+++ spec/support/slack_client_stubbing.rb
@@ -1,8 +1,15 @@
 module Converse
   module Test
     module SlackClientStubbing
+      def stubbed_conversations
+        @stubbed_conversations ||= []
+      end
+
       def stub_slack_chat
-        allow_any_instance_of(::Slack::Web::Client).to receive(:chat_postMessage)
+        allow_any_instance_of(::Slack::Web::Client).to \
+          receive(:chat_postMessage) do |_client, arguments|
+          stubbed_conversations << arguments[:text]
+        end
       end
     end
   end
