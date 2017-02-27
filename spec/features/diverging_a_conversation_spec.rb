966842a2dbde672b8e9111e5c0d6d7a3f35881af
Jamie Wright <jamie@brilliantfantastic.com>
2017-02-27 08:37:33 -0500

    Returning from a diverged conversation

--- spec/features/diverging_a_conversation_spec.rb
+++ spec/features/diverging_a_conversation_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "diverging a conversation" do
   let(:message) { double(:message, user: "U1234", channel: "C1234", team: "T1234") }
 
   before { stub_slack_chat true }
+  after { Converse.clear_templates }
 
   describe "diverging from a simple conversation to a simple conversation" do
     it "runs the first step of the diverged conversation" do
@@ -30,8 +31,25 @@ RSpec.describe "diverging a conversation" do
   end
 
   describe "returning from a diverged conversation" do
-  end
+    it "runs the conversation after the diverge" do
+      Converse::ConversationTemplate.build(:ask_color) do |convo|
+        convo.ask "What color t-shirt do you want?"
+      end.register
 
-  describe "ending within a diverged conversation" do
+      Converse::ConversationTemplate.build(:retrieve_shirt_specs) do |convo|
+        convo.ask "What size t-shirt do you want?" do |convo, response|
+          convo.diverge :ask_color
+          convo.say "Thank you!"
+          convo.end
+        end
+      end.register.start message
+
+      answer_all_questions "XL", "black"
+
+      expect(stubbed_conversations.count).to eq 3
+      expect(stubbed_conversations.first).to eq "What size t-shirt do you want?"
+      expect(stubbed_conversations.second).to eq "What color t-shirt do you want?"
+      expect(stubbed_conversations.third).to eq "Thank you!"
+    end
   end
 end
